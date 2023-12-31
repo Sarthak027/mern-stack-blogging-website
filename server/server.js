b877ed95fe52d74edd7a9c13c80a7deafc6e1679
sarthakprasad012@gmail.com <sarthakprasad012@gmail.com>
2023-12-31 23:39:57 +0530

    1st commit on server side| connected the mongodb db for user auth and setup for req/res

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
+const server = express();
+let PORT = 3000;
+
+server.use(express.json());
+
+
+
+mongoose.connect(process.env.DB_LOCATION ,{
+    autoIndex:true
+})
+
+
+server.post("/signup",(req,res)=>{
+    res.json(req.body)
+})
+
+server.listen(PORT , () =>{
+console.log('listening on port -> ' +PORT);
+
+})
